61d99b8053fceade42bb20e85f6514eee28675fc
nurikk <1525421+nurikk@users.noreply.github.com>
2025-01-23 10:51:43 +0000

    Add tests agaist python 3.12

--- python/setup.py
+++ python/setup.py
@@ -36,6 +36,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.12",
     ],
     dependency_links=[],
     python_requires=">=3.7",
